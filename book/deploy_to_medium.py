8270c410b1b9a0398308a636eb5d2be394273ebc
BexTuychiev <bex@ibexprogramming.com>
2022-06-27 03:38:33 +0500

    deploy: Deploy this week's tricks to Medium

---
+++ book/deploy_to_medium.py
@@ -0,0 +1,16 @@
+import jupyter_to_medium as jtm
+import os
+
+jtm.publish('./drafts/scratch_resources.ipynb',
+            integration_token=os.environ['TOKEN'],
+            pub_name=None,
+            title="",
+            tags=None,
+            publish_status='draft',
+            notify_followers=False,
+            license='all-rights-reserved',
+            canonical_url=None,
+            chrome_path=None,
+            save_markdown=False,
+            table_conversion='chrome'
+            )
